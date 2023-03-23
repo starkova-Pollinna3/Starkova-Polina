4bb54af1dcdc4690b26b2e3853f3ff6d0cb137c2
starkova-Pollinna3 <127307306+starkova-Pollinna3@users.noreply.github.com>
2023-03-23 18:00:18 +0700

    самостоятельная работа
    Добавила самостоятельную за 31.10.2022

---
+++ сам.работа 31.10.js
@@ -0,0 +1,20 @@
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
+
+const distance = 400;
+  time = 20;
+  speed1 = 70;
+  speed2 = 80;
+alert("между автомобилями " + (distance - time * (speed1 + speed2)) + " км");
+
+const edge=2;
+console.log(edge**2);
+let surfaceSquare=6*edge**2;
+console.log(surfaceSquare);
+console.log(edge**3);
