67a735dcd0fab48a2a78e42386b4a38e222778fb
starkova-Pollinna3 <127307306+starkova-Pollinna3@users.noreply.github.com>
2023-03-23 18:09:45 +0700

    + const distance
    Добавила const distance

--- сам.работа 31.10.js
+++ сам.работа 31.10.js
@@ -18,3 +18,9 @@ console.log(edge**2);
 let surfaceSquare=6*edge**2;
 console.log(surfaceSquare);
 console.log(edge**3);
+
+const distance = 400;
+  time = 20;
+  speed1 = 70;
+  speed2 = 80;
+alert("между автомобилями " + (distance - time * (speed1 + speed2)) + " км");
